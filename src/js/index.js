1903e9a667c2c0a822c56957fa9e190559eb2b78
Bruno Délcio Manuel da Silva <128020041+brunodmdev@users.noreply.github.com>
2023-03-17 00:03:51 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,15 @@
-/*
-   OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de funto correspondente
-
-   - passo 1 - dar um jeito de pegar o elemento HTML dos botões
-
-   - passo 2 - dar um jeito de identificar o clique do usuário no botão
-   
-   - passo 3 - desmarcar o botão selecionado anterior
-
-   - passo 4 - marcar o botão clicado como se estivesse selecionado
-
-   - passo 5 - esconder a imagem anterior 
-
-   - passo 6 - fazer aparecer a imagem correspondente ao botão clicado
-*/
-
-// passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const botoesCarrossel = document.querySelectorAll('.botao');
 const imagens = document.querySelectorAll('.imagem');
 
-//passo 2 - dar um jeito de identificar o clique do usuário no botão
 botoesCarrossel.forEach((botao, indice) => {
    botao.addEventListener('click', () => {
 
-      //passo 3 - desmarcar o botão selecionado anterior
       desativarBotaoSelecionado();
 
-      //passo 4 - marcar o botão clicado como se estivesse selecionado
       selecionarBotaoCarrossel(botao);
 
-      //passo 5 - esconder a imagem anterior 
       esconderImagemAtiva();
 
-      //passo 6 - fazer aparecer a imagem correspondente ao botão clicado
       mostrarImagemDeFundo(indice);
    })
 })
